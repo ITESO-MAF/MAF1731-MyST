40613e250fb3dbcc5d3c8c7865ebfeb5b6b0f793
IFFranciscoME <if.francisco.me@gmail.com>
2018-08-23 07:32:19 -0500

    Ultimo commit

---
+++ Notas/Notas_AdminPasiva/Notas_AdminPasiva.R
@@ -0,0 +1,57 @@
+
+# Remover todos los objetos del "Environment"
+rm(list = ls())
+
+# los 0s aceptados antes de expresas una cifra en notaci?n cient?fica
+options("scipen"=100, "digits"=4)
+
+### Cargas librer?as a utilizar
+suppressMessages(library(plotly)) # Graficas interactivas
+suppressMessages(library(Quandl)) # Descargar Precios
+suppressMessages(library(PortfolioAnalytics)) # Teor?a Moderna de Portafolios
+suppressMessages(library(ROI)) # Optimizacion para portafolio
+
+suppressMessages(library(knitr))  # Opciones de documentaci?n + c?digo
+suppressMessages(library(kableExtra)) # Tablas en HTML
+options(knitr.table.format = "html") 
+
+# Cargar el token de QUANDL
+Quandl.api_key("dN9QssXxzTxndaqKUQ_i")
+
+# Funcion para descagar precios
+Bajar_Precios <- function(Columns, Tickers, Fecha_In, Fecha_Fn) {
+  
+  # Funcion para descargar N cantidad de activos desde QUANDL
+  # -- Dependencias: QUANDL
+  # -- Columns : columnas a incluir : character : c("date", "adj_close", ... )
+  # -- Tickers : Tickers o claves de pizarra de los activos : character : "TSLA"
+  # -- Fecha_In : Fecha Inicial : character : "2017-01-02"
+  # -- Fecha_Fn : Fecha Final : character : "2017-08-02"
+  
+  # Peticion para descargar precios
+  Datos <- Quandl.datatable(code = "WIKI/PRICES", qopts.columns=Columns, ticker=Tickers,
+                            date.gte=Fecha_In, date.lte=Fecha_Fn)
+  return(Datos)
+}
+
+# Tickers de accciones y datos a solicitar a QUANDL
+tk <- c("TSLA", "BBY", "HD")
+cs <- c("date", "adj_close")
+
+# Fecha inicial y fecha final
+fs <- c("2015-08-01", "2017-08-01")
+
+# Descargar Precios y Calcular rendimientos
+Datos <- list()
+
+for(i in 1:length(tk))
+  Datos[[i]] <- Bajar_Precios(Columns=cs, Ticker=tk[i], Fecha_In=fs[1], Fecha_Fn=fs[2])
+
+names(Datos) <- tk
+
+for(i in 1:length(tk))
+  Datos[[i]]$adj_close_r <- c(0, diff(log(Datos[[i]]$adj_close)))
+
+Rends <- xts(x = cbind(Datos[[1]]$adj_close_r, Datos[[2]]$adj_close_r, Datos[[3]]$adj_close_r),
+             order.by = Datos[[1]]$date)[-1]
+names(Rends) <- tk
