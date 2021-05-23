961ee129b9938e00eba835a22ce8be6f6208ac03
IFFranciscoME <if.francisco.me@gmail.com>
2021-05-23 00:55:22 -0500

    tests for docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,15 +6,6 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
 import os
 import sys
@@ -36,7 +27,7 @@ release = 'v0.4.1-beta1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.napoleon']
+# extensions = ['sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
